24660627a4f0e51d5373da445b89216700579081
Dan Luu <danluu@gmail.com>
2013-04-16 16:46:28 -0400

    Make code compile

--- juliaparsec.jl
+++ juliaparsec.jl
@@ -3,40 +3,38 @@ type Parser{T}
 
 end
 
-function sequence(f::Function, seq::Array{T,1})
+function sequence{T}(f::Function, seq::Array{T,1})
+    
+end
+
+function branch{T}(f::Function, options::Array{Parser{T},1})
 
 end
 
-function branch(f::Function, options::Array{Parser{T},1})
-
-end
-
-function parse(p::Parser{T},t::Array{T,1})
+function parse{T}(p::Parser{T},t::Array{T,1})
 
 end
 
 
 ## usage example
 
-abstract Tokens
+abstract Token
 type Dog <: Token end
 type Cat <: Token end
 
-function parse_dogcat
+# function parse_dogcat() = branch([ sequence(['c','a','t']) do (_) -> Cat() end, sequence(['d','o','g']) do (_) -> Dog() end]) do (x) -> x end
 
-parse_dogcat = branch([ sequence(['c','a','t']) do (_) -> Cat() end, sequence(['d','o','g']) do (_) -> Dog() end]) do (x) -> x end
+# ## It seems like these two things are not equivalent. Does that seem odd to you?
+# sequence(parse_dogcat, parse_dogcat)
 
-## It seems like these two things are not equivalent. Does that seem odd to you?
-sequence(parse_dogcat, parse_dogcat)
+# sequence(
+#   branch([ sequence(['c','a','t']) do (_) -> Cat() end, sequence(['d','o','g']) do (_) -> Dog() end]),
+#   branch([ sequence(['c','a','t']) do (_) -> Cat() end, sequence(['d','o','g']) do (_) -> Dog() end])) do (x) -> x end
 
-sequence(
-  branch([ sequence(['c','a','t']) do (_) -> Cat() end, sequence(['d','o','g']) do (_) -> Dog() end]),
-  branch([ sequence(['c','a','t']) do (_) -> Cat() end, sequence(['d','o','g']) do (_) -> Dog() end])) do (x) -> x end
+# dogstring = "dog"
+# catstring = "cat"
+# catdogstring = "catdog"
 
-dogstring = "dog"
-catstring = "cat"
-catdogstring = "catdog"
-
-parse(parse_dogcat,dogstring) == Dog()
-parse(parse_dogcat,catstring) == Cat()
-parse(parse_dogcat,catdogstring) == ParsingFailure() 
+# parse(parse_dogcat,dogstring) == Dog()
+# parse(parse_dogcat,catstring) == Cat()
+# parse(parse_dogcat,catdogstring) == ParsingFailure() 
