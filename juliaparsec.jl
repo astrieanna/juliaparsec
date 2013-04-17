a36402e95faea4b6106907942206a3ad58145c24
Dan Luu <danluu@gmail.com>
2013-04-16 21:00:42 -0400

    Kill unused comments

--- juliaparsec.jl
+++ juliaparsec.jl
@@ -91,21 +91,3 @@ myzeroparser = zeroormore(parse_cat)
 @show myzeroparser("dogcat")
 @show myzeroparser("catdog")
 @show myzeroparser("god")
-
-
-# function parse_dogcat() = branch([ sequence(['c','a','t']) do (_) -> Cat() end, sequence(['d','o','g']) do (_) -> Dog() end]) do (x) -> x end
-
-# ## It seems like these two things are not equivalent. Does that seem odd to you?
-# sequence(parse_dogcat, parse_dogcat)
-
-# sequence(
-#   branch([ sequence(['c','a','t']) do (_) -> Cat() end, sequence(['d','o','g']) do (_) -> Dog() end]),
-#   branch([ sequence(['c','a','t']) do (_) -> Cat() end, sequence(['d','o','g']) do (_) -> Dog() end])) do (x) -> x end
-
-# dogstring = "dog"
-# catstring = "cat"
-# catdogstring = "catdog"
-
-# parse(parse_dogcat,dogstring) == Dog()
-# parse(parse_dogcat,catstring) == Cat()
-# parse(parse_dogcat,catdogstring) == ParsingFailure() 
