88161105e3d5b85fa0366783579c4ee2908e4b2a
Leah Hanson <astrieanna@gmail.com>
2013-04-18 23:01:06 -0400

    changed everything to pass position around rather than rest-of-list. This makes everything go from 8.5 to 0.25 seconds. Uncomment the regex version to see the previous time-taken (it was the fastest of the bunch). The code gen in sequence2 is a really bad idea.

--- juliaparsec.jl
+++ juliaparsec.jl
@@ -1,40 +1,39 @@
 function sequence(fs::Array{Function,1})
-    function (xs)
+    function (xs,pos)
         acc = Array(Any,length(fs))
         i = 1
         for f = fs
-            x = f(xs)
+            x = f(xs,pos)
             if x != nothing
-                result, rest = x
-                xs = rest
+                result, pos = x
                 acc[i] = result
                 i += 1
             else
                 return nothing
             end
         end
-        return (acc, xs)
+        return (acc, pos)
     end
 end
 
 function sequence2(fs::Array{Function,1})
-    f = quote function(xs) end end
+    f = quote function(xs,pos) end end
     push!(f.args[2].args[2].args, :(result = Array(Any,length($fs))))
     append!(f.args[2].args[2].args, { quote
-        x = $(fs[i])(xs)
+        x = $(fs[i])(xs,pos)
         if x == nothing
             return nothing
         end
-        result[$i], xs = x
+        result[$i], pos = x
     end for i=1:length(fs)})
-    push!(f.args[2].args[2].args, :(return (result, xs)))
+    push!(f.args[2].args[2].args, :(return (result, pos)))
     return eval(f)
 end
 
 function branch(fs::Array{Function,1})
-    function (xs)
+    function (xs,pos)
         for f = fs
-            x = f(xs)
+            x = f(xs,pos)
             if x != nothing
                 return x
             end
@@ -44,10 +43,10 @@ function branch(fs::Array{Function,1})
 end
 
 function branch2(fs::Array{Function,1})
-  f = quote function(xs) end end
+  f = quote function(xs,pos) end end
   append!(f.args[2].args[2].args, {
     quote
-      r = $f(xs)
+      r = $f(xs,pos)
       if r != nothing
         return r
       end
@@ -58,16 +57,15 @@ function branch2(fs::Array{Function,1})
 end
 
 function zeroormore(f::Function)
-    function (xs)
+    function (xs,pos)
         acc = Array(Any,0)
-        rest = xs
-        x = f(xs)
+        x = f(xs,pos)
         while(x != nothing)            
-            result, rest = x
+            result, pos = x
             push!(acc,result)
-            x = f(rest)
+            x = f(xs,pos)
         end
-        return (acc, rest)
+        return (acc, pos)
      end
 end
 
@@ -78,30 +76,6 @@ abstract CalcToken
 type Plus <: CalcToken end
 type Digit <: CalcToken
   value :: Int
-end
-
-parse_plus(xs) = beginswith(xs,"+") ? (Plus(),xs[2:]) : nothing
-    
-function parse_digit(xs)
-    m = match(r"\d+",xs)
-    return m == nothing ? nothing : (Digit(int(m.match)),xs[length(m.match)+1:])
-end
-
-parse_one_expr = sequence([parse_digit,zeroormore(sequence([parse_plus,parse_digit]))])
-parse_one_expr_codegen = sequence2([parse_digit,zeroormore(sequence2([parse_plus,parse_digit]))])
-function parse_one_expr_hand(str)
-  arr = Array(Any,0)
-  result = parse_digit(str)
-  while result != nothing
-    x,rest = result
-    push!(arr,x)
-    result = parse_plus(rest)
-    if(result == nothing) return arr end
-    x,rest = result
-    push!(arr,x)
-    result = parse_digit(rest)
-  end
-  return nothing 
 end
 
 function parse_char(xs,c,p)
@@ -117,6 +91,37 @@ function parse_num(xs,p)
   end
   return numstr == "" ? nothing : (parseint(numstr),p)
 end
+
+function parse_plus(xs,pos)
+  res = parse_char(xs,'+',pos)
+  if(res == nothing) return nothing end
+  p, pos2 = res
+  return (Plus(),pos2)
+end
+function parse_digit(xs,pos)
+    num = parse_num(xs,pos)
+    if num == nothing return nothing end
+    i,p2 = num
+    return (Digit(i),p2)
+end
+
+parse_one_expr = sequence([parse_digit,zeroormore(sequence([parse_plus,parse_digit]))])
+parse_one_expr_codegen = sequence2([parse_digit,zeroormore(sequence2([parse_plus,parse_digit]))])
+function parse_one_expr_hand(str)
+  arr = Array(Any,0)
+  result = parse_digit(str,1)
+  while result != nothing
+    x,pos = result
+    push!(arr,x)
+    result = parse_plus(str,pos)
+    if(result == nothing) return arr end
+    x,pos = result
+    push!(arr,x)
+    result = parse_digit(str,pos)
+  end
+  return nothing 
+end
+
 function parse_one_expr_strpos(str)
   arr = Array(Any,0)
   pos = 1
@@ -134,11 +139,11 @@ function parse_one_expr_strpos(str)
 end
 
 function interpreter(line)
-    result = parse_one_expr(line)
+    result = parse_one_expr(line,1)
     if result == nothing
         return
     end
-    expr, remainder = result
+    expr, pos = result
     first = expr[1]
     rest = expr[2]
     sum = first.value
@@ -149,11 +154,11 @@ function interpreter(line)
 end
 
 function interpreter_codegen(line)
-    result = parse_one_expr_codegen(line)
+    result = parse_one_expr_codegen(line,1)
     if result == nothing
         return
     end
-    expr, remainder = result
+    expr, pos = result
     first = expr[1]
     rest = expr[2]
     sum = first.value
@@ -209,7 +214,7 @@ function interpreter_strpos(line)
 end
 
 
-numones =  50000
+numones =  500000
 
 @show interpreter("1+2")
 @show interpreter("1+2+3+4")
@@ -227,10 +232,11 @@ onestxt = join(ones(Int,numones),"+")
 @show interpreter_hand("1+2+3+4+5+6+7+8+12345")
 @show median([@elapsed interpreter_hand(onestxt) for x = 1:10])
 
-@show interpreter_regex("1+2")
-@show interpreter_regex("1+2+3+4")
-@show interpreter_regex("1+2+3+4+5+6+7+8+12345")
-@show median([@elapsed interpreter_regex(onestxt) for x = 1:10])
+# this is as slow as all of them used to be (almost 8sec when numones = 50_000)
+# @show interpreter_regex("1+2")
+# @show interpreter_regex("1+2+3+4")
+# @show interpreter_regex("1+2+3+4+5+6+7+8+12345")
+# @show median([@elapsed interpreter_regex(onestxt) for x = 1:10])
 
 @show interpreter_strpos("1+2")
 @show interpreter_strpos("1+2+3+4")
@@ -242,8 +248,38 @@ abstract Token123
 type Dog <: Token123 end
 type Cat <: Token123 end
 
-parse_cat(xs) = beginswith(xs,"cat") ? (Cat(),xs[4:]) : nothing
-parse_dog(xs) = beginswith(xs,"dog") ? (Dog(),xs[4:]) : nothing
+function parse_cat(xs,pos)
+ r = parse_char(xs,'c',pos)
+ if r == nothing return nothing end
+ x,p2 = r
+
+ r = parse_char(xs,'a',p2)
+ if r == nothing return nothing end
+ x,p2 = r
+
+ r = parse_char(xs,'t',p2)
+ if r == nothing
+   return nothing
+ end
+ x,p2 = r
+ return (Cat(),p2)
+end
+function parse_dog(xs,pos)
+ r = parse_char(xs,'d',pos)
+ if r == nothing return nothing end
+ x,p2 = r
+
+ r = parse_char(xs,'o',p2)
+ if r == nothing return nothing end
+ x,p2 = r
+
+ r = parse_char(xs,'g',p2)
+ if r == nothing
+   return nothing
+ end
+ x,p2 = r
+ return (Dog(),p2)
+end
 
 if(false)
 myparser = branch([parse_cat,parse_dog])
@@ -283,24 +319,24 @@ myzeroparser = zeroormore(parse_cat)
 @show myzeroparser("god")
 end
 
-numseq = 100
+numseq = 1000
 cats = fill!(Array(Function,numseq),parse_cat)
 catstext = "cat"^numseq    
 seq1parser = sequence(cats)
 seq2parser = sequence2(cats)
 print("seq / seq2 JIT\n")
-@time seq1parser(catstext)
-@time seq2parser(catstext)
+@time seq1parser(catstext,1)
+@time seq2parser(catstext,1)
 print("seq / seq2 test\n")
-@time seq1parser(catstext)
-@time seq2parser(catstext)
+@time seq1parser(catstext,1)
+@time seq2parser(catstext,1)
 
 branch1parser = branch(cats)
 branch2parser = branch2(cats)
 print("branch / branch2 JIT\n")    
-@time branch1parser(catstext)
-@time branch2parser(catstext)    
+@time branch1parser(catstext,1)
+@time branch2parser(catstext,1)    
 print("branch / branch2 test\n")
-@time branch1parser(catstext)
-@time branch2parser(catstext)
-require("Profile")
+@time branch1parser(catstext,1)
+@time branch2parser(catstext,1)
+#require("Profile")
